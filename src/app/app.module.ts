1caae7708f917a181427f5028c477ddb0cbb99f5
Pyarel Fernandes <43133633+Pyarel@users.noreply.github.com>
2022-02-15 12:09:57 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,14 @@ import { AppComponent } from './app.component';
 import { TaskFormComponent } from './task-form/task-form.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ContainerComponent } from './container/container.component';
-import { NestedComponent } from './nested/nested.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskFormComponent,
     TaskListComponent,
-    ContainerComponent,
-    NestedComponent
+  
   ],
   imports: [
     BrowserModule,
